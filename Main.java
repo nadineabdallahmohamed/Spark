58b24da797d8be6dc048fdae6d86d4460322f06d
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-06-12 23:04:28 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package spark;
+
+import java.io.IOException;
+
+
+public class main {
+    public static void main(String[] args) throws IOException
+    {
+        YoutubeTagWordCount.getTagWordCount();     
+    }
+}
